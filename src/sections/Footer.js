e92411cc8908821d59b27ee5bad66e2a7d34895b
Scotty <Scott.p.mikul@gmail.com>
2020-06-08 18:45:14 -0400

    changed class to className

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -6,13 +6,13 @@ class Footer extends React.Component{
     render(){
         return(
             <div>
-            <h3 class="text-center mt-4" id="contact">Contact</h3>
-            <ul class="row d-flex justify-content-center mt-3 noleftpadding">
-                <li class="col-2"><a href="https://www.linkedin.com/in/scottmikul"><i class="fab fa-linkedin"></i></a></li>
-                <li class="col-2"><a href="https://github.com/ScottMikul"><i class="fab fa-github-square"></i></a></li>
-                <li class="col-2"><a href="https://twitter.com/scottymikul1"><i class="fab fa-twitter"></i></a></li>
-                <li class="col-2"><a href="assets/files/ScottyMikulFullStackDev2020.pdf"><i class="fas fa-file-alt"></i></a>  </li>
-                <li class="col-2"><a href="mailto:scottymikul@gmail.com"><i class="fas fa-envelope"></i></a></li>
+            <h3 className="text-center mt-4" id="contact">Contact</h3>
+            <ul className="row d-flex justify-content-center mt-3 noleftpadding">
+                <li className="col-2"><a href="https://www.linkedin.com/in/scottmikul"><i className="fab fa-linkedin"></i></a></li>
+                <li className="col-2"><a href="https://github.com/ScottMikul"><i className="fab fa-github-square"></i></a></li>
+                <li className="col-2"><a href="https://twitter.com/scottymikul1"><i className="fab fa-twitter"></i></a></li>
+                <li className="col-2"><a href="assets/files/ScottyMikulFullStackDev2020.pdf"><i className="fas fa-file-alt"></i></a>  </li>
+                <li className="col-2"><a href="mailto:scottymikul@gmail.com"><i className="fas fa-envelope"></i></a></li>
             </ul>
             </div>
 
